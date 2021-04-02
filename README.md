d58e555fb7dc38b85b4b8d2cf4e7e0d46f754bbf
Avnishthedeveloper <avnish.kumar5894@gmail.com>
2021-04-03 00:28:44 +0530

    Added requirements file and Modified README.md

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Directory_tree
+
+# Install necessary libraries:
+
+pip install -r requirements.txt
+
+# Help options to run the cmd directory tree tool:
+
+python3 tree.py -h
+
+usage: tree.py [-h] [-d DIR_PATH] [-l LEVEL] [-ld LIMIT_TO_DIRECTORIES]
+               [-ll LENGTH_LIMIT]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -d DIR_PATH, --dir_path DIR_PATH
+                        base directory path for the tree
+  -l LEVEL, --level LEVEL
+                        level of the tree
+  -ld LIMIT_TO_DIRECTORIES, --limit_to_directories LIMIT_TO_DIRECTORIES
+                        limit_to_directories
+  -ll LENGTH_LIMIT, --length_limit LENGTH_LIMIT
+                        length_limit
+
+# Run
+
+python3 tree.py -d <dir_path> -l <level>
+
+# Example
+
+python3 tree.py -d './' -l 2
--- tree.py
+++ tree.py
@@ -1,67 +1,5 @@
 ## tree structure with diff. color for files and directories and additional parameter for level of tree
 
-# prefix components:
-# space =  '    '
-# branch = '│   '
-# # pointers:
-# tee =    '├── '
-# last =   '└── '
-# import os
-
-# def list_files(startpath, allowed_level):
-# 	# print(os.walk(startpath))
-# 	for root, dirs, files in os.walk(startpath):
-# 		# print(root, dirs, files)
-# 		level = root.replace(startpath, '').count(os.sep)
-# 		# print(level)
-# 		if level <= allowed_level:
-# 			indent = ' ' * 4 * (level)
-# 			print('{}{}/'.format(indent, os.path.basename(root)))
-# 			subindent = ' ' * 4 * (level + 1)
-# 			for f in files:
-# 				print('{}{}'.format(subindent, f))
-
-# # current_path = os.getcwd()
-# current_path = '/media/avnish/12CE1B5ACE1B3587/'
-# # current_path = '/media/avnish/12CE1B5ACE1B3587/3rd year'
-# list_files(current_path,0)
-
-## Second Method of tree
-# from pathlib import Path
-
-# # prefix components:
-# space =  '    '
-# branch = '│   '
-# # pointers:
-# tee =    '├── '
-# last =   '└── '
-
-
-# def tree(dir_path: Path, prefix: str=''):
-#     """A recursive generator, given a directory Path object
-#     will yield a visual tree structure line by line
-#     with each line prefixed by the same characters
-#     """    
-#     contents = list(dir_path.iterdir())
-#     # contents each get pointers that are ├── with a final └── :
-#     pointers = [tee] * (len(contents) - 1) + [last]
-#     for pointer, path in zip(pointers, contents):
-#         yield prefix + pointer + path.name
-#         if path.is_dir(): # extend the prefix and recurse:
-#             extension = branch if pointer == tee else space 
-#             # i.e. space because last, └── , above so no more |
-#             yield from tree(path, prefix=prefix+extension)
-
-# # # and now:
-# current_path = '/media/avnish/12CE1B5ACE1B3587/3rd year'
-# # list_files(current_path,0)
-
-# # for line in tree(Path.home() / '.'):
-# for line in tree(Path.home()/current_path):
-#     print(line)
-
-
-#### Third way for tree file structure
 from pathlib import Path
 from itertools import islice
 from colorama import Fore, Back, Style
@@ -104,10 +42,6 @@ def tree(dir_path: Path, level: int=-1, limit_to_directories: bool=False,
         print(f'... length_limit, {length_limit}, reached, counted:')
     print(f'\n{directories} directories' + (f', {files} files' if files else ''))
 
-# current_path = '/media/avnish/12CE1B5ACE1B3587/3rd year'
-# # current_path = '/media/avnish/12CE1B5ACE1B3587/3rd year'
-# tree(current_path,5)
-
 if __name__ == '__main__':
 
 	# construct the argument parser and parse command line arguments
@@ -115,7 +49,7 @@ if __name__ == '__main__':
 	ap.add_argument("-d", "--dir_path", type=str, help="base directory path for the tree")
 	ap.add_argument("-l", "--level", type=int, help="level of the tree")
 	ap.add_argument("-ld", "--limit_to_directories", type=bool, default=False,
-		help="# limit_to_directories")
+		help="limit_to_directories")
 	ap.add_argument("-ll", "--length_limit", type=int, default=1000, help="length_limit")
 	# args = vars(ap.parse_args())
 	args = ap.parse_args()
@@ -125,10 +59,3 @@ if __name__ == '__main__':
 	else:
 		tree(dir_path=args.dir_path, level=args.level, limit_to_directories=args.limit_to_directories,
          length_limit=args.length_limit)
-
-
-## Fourth way
-# for path, dirs, files in os.walk(os.getcwd()):
-#   print (path)
-#   for f in files:
-#     print (f) 
